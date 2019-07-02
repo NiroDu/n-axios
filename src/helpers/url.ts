77d7ca5fdbc49b6aadb67def8a993a26cab227fd
Du <pei.du@ximalaya.com>
2019-07-02 18:59:50 +0800

    feat: basic send function...
    1. basic send function
    2. handle request and respond
    3. handle headers
    4. trimming url

---
+++ src/helpers/url.ts
@@ -0,0 +1,56 @@
+import { isDate, isPlainObject } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
+      return
+    }
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else if (isPlainObject(val)) {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    // url上，去除#之后的内容
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    // url上有?就直接‘&参数‘，否则加?
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
